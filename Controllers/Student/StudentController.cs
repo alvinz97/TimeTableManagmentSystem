0c5123e0871216143e09bf18e20162ebe3ef157e
IsuruHareen <isuruhareen1@gmail.com>
2021-06-04 23:38:10 -0700

    Implemented the storeStudent

--- Controllers/Student/StudentController.cs
+++ Controllers/Student/StudentController.cs
@@ -22,5 +22,41 @@ namespace TimeTableManagmentSystem.Controllers.Student
             dataGridView.DataSource = dataTable;
             connection.Close();
         }
+
+        public static void Store(Models.Student student)
+        {
+            string query = "INSERT INTO student " +
+                "(Year, Semester, Program, GroupNo, GroupID, SubGroupNo, SubGroupID, CreatedAt, UpdatedAt) " +
+                "VALUES " +
+                "(@Year, @Semester, @Program, @GroupNo, @GroupID, @SubGroupNo, @SubGroupID, @CreatedAt, @UpdatedAt)";
+
+            SqlConnection connection = Connection.GetConnection();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@Year", Convert.ToInt16(student.Year));
+            command.Parameters.AddWithValue("@Semester", Convert.ToInt16(student.Semester));
+            command.Parameters.AddWithValue("@Program", student.Program);
+            command.Parameters.AddWithValue("@GroupNo", Convert.ToInt16(student.GroupNo));
+            command.Parameters.AddWithValue("@GroupID", student.GroupID);
+            command.Parameters.AddWithValue("@SubGroupNo", Convert.ToInt16(student.SubGroupNo));
+            command.Parameters.AddWithValue("@SubGroupID", student.SubGroupID);
+            command.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+            command.Parameters.AddWithValue("@UpdatedAt", DateTime.Now);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Successfully Added", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Error Occur. \n" + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
