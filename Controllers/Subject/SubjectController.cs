46239cb0ab16987efab600ecc9afbe7b7fd73ad0
IsuruHareen <isuruhareen1@gmail.com>
2021-06-05 04:31:58 -0700

    Implemented update function in Subject

--- Controllers/Subject/SubjectController.cs
+++ Controllers/Subject/SubjectController.cs
@@ -75,5 +75,45 @@ namespace TimeTableManagmentSystem.Controllers.Subject
                 connection.Close();
             }
         }
+        public static void Update(Models.Subject subject, string id)
+        {
+            string query = "UPDATE subject SET " +
+                "Year = @Year, " +
+                "Semester = @Semester, " +
+                "SubjectName = @SubjectName, " +
+                "SubjectCode = @SubjectCode, " +
+                "LectureHours = @LectureHours, " +
+                "TutorialHours = @TutorialHours, " +
+                "LabHours = @LabHours, " +
+                "EvaluationHours = @EvaluationHours, " +
+                "UpdatedAt = @UpdatedAt " +
+                "WHERE id =" + id;
+
+            SqlConnection connection = Connection.GetConnection();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Year", Convert.ToInt16(subject.Year));
+            command.Parameters.AddWithValue("@Semester", Convert.ToInt16(subject.Semester));
+            command.Parameters.AddWithValue("@SubjectName", subject.SubjectName);
+            command.Parameters.AddWithValue("@SubjectCode", subject.SubjectCode);
+            command.Parameters.AddWithValue("@LectureHours", Convert.ToInt16(subject.LectureHours));
+            command.Parameters.AddWithValue("@TutorialHours", Convert.ToInt16(subject.TutorialHours));
+            command.Parameters.AddWithValue("@LabHours", Convert.ToInt16(subject.LabHours));
+            command.Parameters.AddWithValue("@EvaluationHours", Convert.ToInt16(subject.EvaluationHours));
+            command.Parameters.AddWithValue("@UpdatedAt", DateTime.Now);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Successfully Updated", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Error Occur. \n" + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
